71de8a85e6de230a90a1a1cede4eea428e08a7f3
qvuongg <quocvuong22052003@gmail.com>
2024-07-31 23:03:08 +0700

    Install beautiful code

--- src/index.js
+++ src/index.js
@@ -23,9 +23,9 @@ app.use(express.json());
 //Template engine
 app.engine(
     "hbs",
-       engine({
-        extname   : ".hbs",
-    })   ,
+    engine({
+        extname: ".hbs",
+    }),
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
