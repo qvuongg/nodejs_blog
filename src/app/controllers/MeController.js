590b3970c0f1cbe4967e64afe87e1c551e9b7b74
qvuongg <quocvuong22052003@gmail.com>
2024-08-20 08:09:54 +0700

    update sort

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,9 +2,20 @@ const Course = require('../models/Course')
 const { mutipleMongooseToObject } = require('../../util/mongoose')
 
 class MeController {
-    // Get /
+    // Get /me/stored/courses
     storedCourses(req, res, next) {
-        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+        let courseQuery = Course.find({})
+
+
+        if (req.query.hasOwnProperty('_sort')){
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type
+            })
+            
+        }
+
+
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then(([courses,deletedCount]) =>
                 res.render('me/stored-courses',{
                     deletedCount,
